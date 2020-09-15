d5c1c137e202565d5ed29191c90cbbbedef2f2fa
Greg Mall <gregmall157@gmail.com>
2020-09-15 11:30:45 -0700

    forgot to commit this change, added scraped book count

--- scrapper.js
+++ scrapper.js
@@ -5,6 +5,9 @@ const store = require('./lib/store');
 
 request()
   .then(document => parser(document))
-  .then(books => store(books));
+  .then(books => store(books))
+  .then(books => console.log(books.length));
+
+
  
 
