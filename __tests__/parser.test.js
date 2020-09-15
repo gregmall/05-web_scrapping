55949f59addf1360e9cdd079a69708d3cfa3a4d7
Greg Mall <gregmall157@gmail.com>
2020-09-14 17:00:09 -0700

    model created

--- __tests__/parser.test.js
+++ __tests__/parser.test.js
@@ -11,16 +11,23 @@ describe('parser function', () => {
       {
         image: 'media/cache/2c/da/2cdad67c44b002e7ead0cc35693c0e8b.jpg',
         rating: 'Three',
-        title: 'A Light in the ...',
+        title: 'A Light in the Attic',
         price: '£51.77',
-        available: null
+        available: true
       },
       {
         image: 'media/cache/26/0c/260c6ae16bce31c8f8c95daddd9f4a1c.jpg',
         rating: 'One',
         title: 'Tipping the Velvet',
         price: '£53.74',
-        available: null
+        available: true
+      },
+      {
+        image: 'media/cache/3e/ef/3eef99c9d9adef34639f510662022830.jpg',
+        rating: 'One',
+        title: 'Soumission',
+        price: '£50.10',
+        available: true
       }
 
     ]));
