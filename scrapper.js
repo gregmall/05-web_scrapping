3d053c083c4c39e2137335768002f0b0537366b4
Greg Mall <gregmall157@gmail.com>
2020-09-14 17:57:34 -0700

    finished scrape

---
+++ scrapper.js
@@ -0,0 +1,9 @@
+const request = require('./lib/request');
+const parser = require('./lib/parser');
+const store = require('./lib/store');
+
+
+request()
+  .then(document => parser(document))
+  .then(books => store(books));
+
